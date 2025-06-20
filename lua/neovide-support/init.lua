1ba9fd7d2d2e966b3bcb0d63b317e5b433fa812c
jinxin <jinxinkingsir@gmail.com>
2025-06-21 00:30:23 +0800

    Update init.lua

--- lua/neovide-support/init.lua
+++ lua/neovide-support/init.lua
@@ -14,7 +14,7 @@ M.options = nil
 
 function M.config(options)
 	M.options = vim.tbl_deep_extend("force", {}, M.defaults, options or {})
-	local opts = require('neovide-support.config').extend(options)
+	local opts = M.extend(options)
 	if vim.loop.os_uname().sysname == "Windows_NT" then
 		vim.g.neovide_title_background_color =
 				string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
